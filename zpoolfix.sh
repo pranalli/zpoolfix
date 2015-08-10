b0038fab956fef726d89c5ddeed7aca55c0ba09c
Pasquale Ranalli <pranalli@gmail.com>
2015-08-10 08:37:29 -0400

    Added documentation

--- zpoolfix.sh
+++ zpoolfix.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This is designed for the purposes of "fixing" an overly-full ZFS zpool, but, more generally, is just a script that "psuedo-deletes" files by
+# overwriting their contents with a single byte.  Once enough space is cleaned up by using this script, you can start using normal rm to begin
+# removing the files overwritten by the script, and any further you choose to either remove or move away from the zpool to regain proper
+# functionality and performance. 
+
 DIR=${1}
 if [ -z ${DIR} ] 
 	then
