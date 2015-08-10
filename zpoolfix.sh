ab162decae8a3e49d39064c267756b5e6b0812fa
Pasquale Ranalli <pranalli@gmail.com>
2015-08-10 08:51:17 -0400

    Add support for specifying path or files with or without wildcards

--- zpoolfix.sh
+++ zpoolfix.sh
@@ -3,24 +3,28 @@
 # This is designed for the purposes of "fixing" an overly-full ZFS zpool, but, more generally, is just a script that "psuedo-deletes" files by
 # overwriting their contents with a single byte.  Once enough space is cleaned up by using this script, you can start using normal rm to begin
 # removing the files overwritten by the script, and any further you choose to either remove or move away from the zpool to regain proper
-# functionality and performance. 
+# functionality and performance.  Use this script with extreme caution!
 
-DIR=${1}
-if [ -z ${DIR} ] 
-	then
-		echo "Proper use: zpoolfx [PATH]"
-		exit -1 
+if [ -z ${1} ] 
+    then
+        echo "Proper use: zpoolfx [FILES (wildcards allowed)]"
+        exit -1 
 fi
 
-cd ${DIR}
-for i in *
-	do
-		if [ -d ${i} ]
-			then
-				echo "${i} is a directory"
-		fi
-		if [ -f ${i} ]     
-			then 
-				echo "${i} is a file"
-		fi
-	done 
+FILES=${@}
+for i in ${FILES}
+    do
+
+        if [ -d ${i} ]
+            then
+                echo "${i} is a directory"
+        fi
+
+        if [ -f ${i} ]     
+            then 
+                echo "${i} is a file"
+        fi
+
+    done 
+
+exit 0
